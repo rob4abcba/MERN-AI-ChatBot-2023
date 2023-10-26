936cda24452f1fa12f88bb8008bd5617c8568135
rob <rob_abcba@yahoo.com>
2023-10-26 15:08:33 -0700

    Move import express and const app = express() to new app.ts file.  Create ./db/connection.js file, import it and execute it here in index.ts. These changes automatically also occur in dist folders index.js and index.js.map files.

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,19 @@
-import express from "express";
-const app = express();
-app.listen(5000, () => console.log("Server Open on PORT =", 5000));
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+// app.post("/user/:userid", (req, res, next) => {
+//   console.log("req.body.name = " + req.body.name);
+//   console.log("req.params.userid = " + req.params.userid);
+//   return res.send(
+//     "req.body.name = " +
+//       req.body.name +
+//       "\nreq.params.userid = " +
+//       req.params.userid
+//   );
+// });
+// connection & listener
+connectToDatabase()
+    .then(() => {
+    app.listen(5000, () => console.log("Server Open on PORT =", 5000));
+})
+    .catch((err) => console.log("connectToDatabase: err =", err));
 //# sourceMappingURL=index.js.map
